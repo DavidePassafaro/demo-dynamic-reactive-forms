498232e83658b177a3724c3d8fb5d2ada758f99f
Davide Passafaro <davide.passafaro@conte.it>
2023-11-26 17:48:24 +0100

    created draft template for simple-form-page

--- src/app/features/simple/simple-form-page.component.ts
+++ src/app/features/simple/simple-form-page.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckboxListComponent, InputComponent } from '@drf-shared';
+import {
+  ReactiveFormsModule,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'drf-simple-form-page',
   templateUrl: './simple-form-page.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    InputComponent,
+    CheckboxListComponent,
+    ReactiveFormsModule,
+  ],
 })
-export class SimpleFormPageComponent {}
+export class SimpleFormPageComponent {
+  public name = new UntypedFormControl(null, Validators.required);
+  public skills = new UntypedFormControl(['react'], Validators.required);
+
+  public formGroup = new UntypedFormGroup({
+    name: this.name,
+    skills: this.skills,
+  });
+
+  constructor() {
+    this.formGroup.controls['name'].valueChanges.subscribe(console.log);
+  }
+}
