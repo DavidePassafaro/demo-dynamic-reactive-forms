d0896e1123576b05128f69e6aa4ba8e5f8614c0b
DavidePassafaro <passafaro@hotmail.it>
2023-11-27 12:07:36 +0100

    improved homepage

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'drf-homepage',
   templateUrl: './homepage.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class HomepageComponent {}
